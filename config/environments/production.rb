6b5a62348fd15ebac5778ac4afaf483028e04ee5
janel lucia <thejanellucia@gmail.com>
2018-05-09 16:31:26 -0400

    fixed mailer issue in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'listed-eleven.herokuapp.com' }
 end
